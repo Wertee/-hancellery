31813143155664d8963a3a3b50faedb7587a5c68
Александр Павлов <Waterbin1993@gmail.com>
2021-12-02 13:26:00 +0300

    Add project files.

---
+++ PetProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
