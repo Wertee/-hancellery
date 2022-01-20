e21b858dcb0e2b0b881b06a42482acc21626243d
Александр Павлов <Waterbin1993@gmail.com>
2022-01-20 14:41:25 +0300

    Added new fields to User

---
+++ PetProjectMVC/Migrations/GameStoreIdentity/20220120103539_UserMigration2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetProjectMVC.Migrations.GameStoreIdentity
+{
+    public partial class UserMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserLastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserLastName",
+                table: "AspNetUsers");
+        }
+    }
+}
